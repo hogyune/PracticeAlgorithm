d61a5c8fbf02009aad75c51329ebce16682c97a3
hogyune <34997293+hogyune@users.noreply.github.com>
2018-07-10 10:03:31 +0900

    Add files via upload

---
+++ baekjoon/prac.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std;
+
+int Ary[41] = { 0 };
+
+int fibonacci(int n) {
+    if (Ary[n] > 0) return Ary[n];
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+    return Ary[n] = fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+ int main() {
+     int K, N;
+     scanf("%d", &K);
+    for (int i = 0; i < K; i++) {
+		scanf("%d", &N);
+		if (N == 0) printf("1 0\n");
+	    else printf("%d %d\n", fibonacci(N - 1), fibonacci(N));
+	}
+	return 0;
+}
